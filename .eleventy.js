fa9fea4d484cf50dd811853dd771b00e26b0a945
SFJT <santiagojuarez.dev@gmail.com>
2022-03-15 20:19:16 +0100

    First Commit

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const { DateTime } = require("luxon");
+
+
+module.exports = function(eleventyConfing){
+  
+  eleventyConfing.addPassthroughCopy('./src/style.css');
+  eleventyConfing.addPassthroughCopy('./src/assets');
+  
+  eleventyConfing.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
+}
